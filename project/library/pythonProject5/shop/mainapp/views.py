f629ec4889bb7b915c34ea0f13043d0bd7ca67c1
kuanysh.zhaksylyk <kuanysh.zhk@gmail.com>
2021-11-19 14:58:17 +0300

    28 lesson doesn't work

--- project/library/pythonProject5/shop/mainapp/views.py
+++ project/library/pythonProject5/shop/mainapp/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-def test_view(request):
-    return render(request, 'base.html', {})
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView, View
 
-from .models import  Notebook, Smartphone
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
+from .mixins import CategoryDetailMixin, CartMixin
 
-class ProductDetailView(DetailView):
+class BaseView(CartMixin,View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page('notebook','smartphone')
+        context = {
+            'categories': categories,
+            'products': products,
+            'cart': self.cart
+        }
+        return render(request, 'base.html', context)
+        #categories = Category.objects.get_category_for_left_sidebar()
+  #  return render(request, 'base.html', {'categories': categories})
+
+#def test_view(request):
+ #   categories = Category.objects.get_category_for_left_sidebar()
+  #  return render(request, 'base.html', {'categories': categories})
+
+
+class ProductDetailView(CategoryDetailMixin,DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -22,3 +43,75 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
+class CategoryDetailView(CartMixin,CategoryDetailMixin, DetailView):
+
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
+    slug_url_kwarg = 'slug'
+
+class AddToCartView(CartMixin,View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product,created = CartProduct.objects.get_or_create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        if created:
+             self.cart.products.add(cart_product)
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Товар успешно добавлен")
+        return HttpResponseRedirect('/cart/')
+
+
+class DeleteFromCartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        self.cart.products.remove(cart_product)
+        cart_product.delete()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Товар успешно удален")
+        return HttpResponseRedirect('/cart/')
+
+class ChangeQTYView(CartMixin, View):
+
+    def post(self,request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        qty = int(request.POST.get('qty'))
+        cart_product.qty = qty
+        cart_product.save()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Кол-во успешно изменено ")
+        return HttpResponseRedirect('/cart/')
+
+class CartView(CartMixin,View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+
+        context = {
+            'cart':self.cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
+
